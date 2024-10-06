0dc08fc285b0b149402fa9afb7c8d0a3e2dae4d2
Viktor5460 <vips@MSI>
2024-10-06 18:08:20 +0300

    Everything done NOWWWWWWW..... how looooong....

--- 2_merge/result/GOAL.sh
+++ 2_merge/result/GOAL.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-gcc 1_bubble.c -o 1
+gcc 2_merge.c -o 1
 ./1
 python3 main.py
 touch file_list.txt
 file_list.py
-ffmpeg -f concat -i file_list.txt -i zvuk.mp3 1_bubble_output.mp4
-
+ffmpeg -f concat -i file_list.txt -i zvuk.mp3 2_merge_output.mp4
--- 3_quicksort/GOAL.sh
+++ 3_quicksort/GOAL.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-gcc 1_bubble.c -o 1
+gcc 3_quicksort.c -o 1
 ./1
 python3 main.py
 touch file_list.txt
 file_list.py
-ffmpeg -f concat -i file_list.txt -i zvuk.mp3 1_bubble_output.mp4
-
+ffmpeg -f concat -i file_list.txt -i zvuk.mp3 3_quicksort_output.mp4
